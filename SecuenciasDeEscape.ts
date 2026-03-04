80fca76717cbba53bed4b5fd0b1d00b070e2647e
Michael665 <maicolbeltran885@gmail.com>
2026-03-04 18:31:20 -0500

    secuencias de escape

---
+++ SecuenciasDeEscape.ts
@@ -0,0 +1,10 @@
+// secuencias de escape
+
+"\'" // comillas simples ejemplo: 'Hola, soy un texto con comillas simples: \'Esto es una comilla simple\''
+"\"" // comillas dobles ejemplo: "Hola, soy un texto con comillas dobles: \"Esto es una comilla doble\""
+"\\" // barra invertida ejemplo: "Hola, soy un texto con una barra invertida: \\ Esto es una barra invertida"
+"\n" // salto de linea ejemplo: "Hola, soy un texto con un salto de linea: \nEsto es una nueva linea"
+"\r" // tabulacion     ejemplo: "Hola, soy un texto con una tabulacion: \rEsto es una tabulacion"
+"\t" // salto de linea ejemplo: "Hola, soy un texto con un salto de linea: \tEsto es una nueva linea con tabulacion"
+"\b" // retroceso      ejemplo: "Hola, soy un texto con un retroceso: \bEsto es un retroceso"
+"\f" // avance de pagina ejemplo: "Hola, soy un texto con un avance de pagina: \fEsto es un avance de pagina"
