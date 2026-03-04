48a73ca5104065e33efc4ad5a87d9d9e1dd49666
Michael665 <maicolbeltran885@gmail.com>
2026-03-04 18:41:37 -0500

    Concatenar y longitud de  cadena

---
+++ Concatenar.ts
@@ -0,0 +1,14 @@
+let NombreCompleto =  "maicol" + " " + "beltran";
+
+console.log(NombreCompleto);
+let Saludo = "hola yo soy " + NombreCompleto;
+console.log(Saludo);
+
+let Frase1 = "Me gusta la nube de azure";
+let Frase2 = "porque es muy facil de usar";
+let FraseCompleta = Frase1 + " " + Frase2;
+console.log(FraseCompleta);
+
+
+let MiCadena = "Hola, soy una cadena de texto";
+console.log(MiCadena.length); // Esto imprimirá la longitud de la cadena de texto
